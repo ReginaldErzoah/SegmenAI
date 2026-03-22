118a06edde2c8ccdc92d2a5e792f26344393b6d0
Reginald Erzoah <147485458+ReginaldErzoah@users.noreply.github.com>
2026-03-22 20:58:03 +0000

    Updates

--- SegmenAI_terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_name" {
-  description = "Name of S3 bucket for SegmenAI models"
-  value       = aws_s3_bucket.segmenai_bucket.bucket
-}
-
-output "iam_role_arn" {
-  description = "ARN of IAM role for Streamlit app"
-  value       = aws_iam_role.segmenai_role.arn
-}
